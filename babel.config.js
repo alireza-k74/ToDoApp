13947301cea1632f9e2b0346b43b0448ccd6e49e
alireza.karami <alirezakaram.k74@gmail.com>
2024-12-25 12:12:26 +0330

    add some packages

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.android.js',
+          '.android.tsx',
+          '.ios.js',
+          '.ios.tsx',
+        ],
+        root: ['.'],
+        alias: {
+          '~': './src',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
